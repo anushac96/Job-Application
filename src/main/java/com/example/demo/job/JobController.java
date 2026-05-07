a307c9616470a51d89fe505776e822a846cca5eb
anushac96 <anushac96@gmail.com>
2026-05-06 23:51:23 -0500

    get post jobs

--- src/main/java/com/example/demo/job/JobController.java
+++ src/main/java/com/example/demo/job/JobController.java
@@ -1,9 +1,25 @@
 package com.example.demo.job;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class JobController {
-	public List<Job> findAllJobs(){
+	private List<JobModule> jobs = new ArrayList<>();
+	
+	@GetMapping("/jobs")
+	public List<JobModule> findAllJobs(){
 		return jobs;
+	}
+	
+	@PostMapping("/jobs")
+	public String createJob(@RequestBody JobModule jobModule){
+		jobs.add(jobModule);
+		return "Job added successfully";
 	}
 }
